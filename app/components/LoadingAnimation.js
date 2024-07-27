f08dab85038bb0bc9f8d15939bd3d61d931a94ab
mhdaslmnzr <mhdaslmnzr@gmail.com>
2024-07-27 09:56:18 +0530

    minor edits + final

--- app/components/LoadingAnimation.js
+++ app/components/LoadingAnimation.js
@@ -4,7 +4,7 @@ const LoadingAnimation = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <h2 className="mb-4 text-2xl font-semibold text-gray-900 animate-pulse">
-        Loading ICREP TC Data
+        ICREP TC Generator Loading
         <span className="dots">
           <span className="dot">.</span>
           <span className="dot">.</span>
